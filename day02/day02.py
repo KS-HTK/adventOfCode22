4cc46fc1e85e110a7bff993c489044d38271c356
KS_HTK <HKoertge@gmx.de>
2022-12-02 06:39:15 +0100

    cleaned up formatting

--- day02/day02.py
+++ day02/day02.py
@@ -20,38 +20,36 @@ with open('day02/input', 'r', encoding='utf-8') as f:
   input = input.rstrip().split('\n')
   input = list(map(lambda s: s.split(' '), input))
 
+
 # Part 1:
-def part1(round: List[List[str]]=None):
+def part1(round: List[List[str]] = None):
   my_score: int = 0
   for [op, me] in round:
     my_score += get_round_score(lookup[me], lookup[op])
-    
-
   return my_score
 
 
 # Part 2:
-def part2(round: List[List[str]]=None):
+def part2(round: List[List[str]] = None):
   my_score: int = 0
   for [op, outcome] in round:
     op = lookup[op]
-    me = op if outcome=='Y' else ((op+2) if outcome=='X' else (op+1))
-    me = me if me<=3 else me-3
+    me = op if outcome == 'Y' else ((op+2) if outcome == 'X' else (op+1))
+    me = me if me <= 3 else me-3
     my_score += get_round_score(me, op)
-
   return my_score
 
 
-def get_round_score(me:int, op:int)->int:
-    if (op==me):
-      # Draw
-      return me+3
-    if (me==op+1 or me==1 and op==3):
-      # me Win
-      return me+6
-    else:
-      # me Lose
-      return me
+def get_round_score(me: int, op: int) -> int:
+  if (op == me):
+    # Draw
+    return me+3
+  if (me == op+1 or me == 1 and op == 3):
+    # me Win
+    return me+6
+  else:
+    # me Lose
+    return me
 
 
 if __name__ == "__main__":
