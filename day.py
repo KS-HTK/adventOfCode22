94a76780433b0567ad300db2a872ecb8ae6b84c4
KS_HTK <HKoertge@gmx.de>
2022-12-03 06:49:46 +0100

    updated day.py

--- day.py
+++ day.py
@@ -6,19 +6,18 @@ from typing import List
 input: List[str] = None
 # TODO: change the path of input file!
 with open('day01/input', 'r', encoding='utf-8') as f:
+  input = f.read().rstrip().split('\n')
   input = map(lambda s: s.strip(), f.readlines())
-
-numbers: List[int] = list(map(int, input))
 
 # Part 1:
 def part1(input=None):
-  pass
+  return 0
 
 # Part 2:
 def part2(input=None):
-  pass
+  return 0
 
 
 if __name__ == "__main__":
-  print("Part 1:", part1(numbers))
-  print("Part 2:", part2(numbers))
+  print("Part 1:", part1(input))
+  print("Part 2:", part2(input))
