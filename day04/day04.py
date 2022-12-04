10ae05812ea89296868278e7a926773f341fce7c
KS_HTK <HKoertge@gmx.de>
2022-12-04 06:49:55 +0100

    solved day04 (cleaned code)

--- day04/day04.py
+++ day04/day04.py
@@ -9,13 +9,14 @@ with open('day04/input', 'r', encoding='utf-8') as f:
   input = list(map(lambda l: list(map(lambda s: s.split('-'), l)), input))
   input = [[[int(j) for j in i] for i in x] for x in input]
 
+
 # Part 1:
-def part1(input: List[List[List[int]]]=None):
+def part1(input: List[List[List[int]]] = None):
   return solve(input, range_subset)
 
 
 # Part 2:
-def part2(input: List[List[List[int]]]=None):
+def part2(input: List[List[List[int]]] = None):
   return solve(input, range_overlap)
 
 
@@ -26,10 +27,12 @@ def solve(input: List[List[List[int]]], func):
       res += 1
   return res
 
-def range_subset(r1:List[int], r2:List[int]):
+
+def range_subset(r1: List[int], r2: List[int]):
   x1, x2 = r1
   y1, y2 = r2
   return x1 <= y1 and y2 <= x2
+
 
 def range_overlap(r1: List[int], r2: List[int]):
   x1, x2 = r1
@@ -38,5 +41,5 @@ def range_overlap(r1: List[int], r2: List[int]):
 
 
 if __name__ == "__main__":
-  print("Part 1:", part1(input))
-  print("Part 2:", part2(input))
+    print("Part 1:", part1(input))
+    print("Part 2:", part2(input))
