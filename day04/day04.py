3f389ca43e974b448decee97e29b96020213f60a
KS_HTK <HKoertge@gmx.de>
2022-12-05 10:54:22 +0100

    updated day04

--- day04/day04.py
+++ day04/day04.py
@@ -10,16 +10,6 @@ input = list(map(lambda l: list(map(lambda s: s.split('-'), l)), input))
 input = [[[int(j) for j in i] for i in x] for x in input]
 
 
-# Part 1:
-def part1(input: List[List[List[int]]] = None):
-  return solve(input, range_subset)
-
-
-# Part 2:
-def part2(input: List[List[List[int]]] = None):
-  return solve(input, range_overlap)
-
-
 def solve(input: List[List[List[int]]], func):
   res: int = 0
   for lst in input:
@@ -28,18 +18,14 @@ def solve(input: List[List[List[int]]], func):
   return res
 
 
-def range_subset(r1: List[int], r2: List[int]):
-  x1, x2 = r1
-  y1, y2 = r2
+def range_subset([x1, x2]: List[int], [y1, y2]: List[int]):
   return x1 <= y1 and y2 <= x2
 
 
-def range_overlap(r1: List[int], r2: List[int]):
-  x1, x2 = r1
-  y1, y2 = r2
+def range_overlap([x1, x2]: List[int], [y1, y2]: List[int]):
   return x1 <= y2 and y1 <= x2
 
 
 if __name__ == "__main__":
-    print("Part 1:", part1(input))
-    print("Part 2:", part2(input))
+    print("Part 1:", solve(input, range_subset))
+    print("Part 2:", solve(input, range_overlap))
