667f2a00d334d652151781f9832695fa43109d75
KS_HTK <HKoertge@gmx.de>
2022-12-05 22:35:59 +0100

    updated day.py and  .gitignore

--- day.py
+++ day.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 
-"""Basic Python file for each day"""
 from typing import List
 
-input = None
-# TODO: change the path of input file!
-with open('day01/input', 'r', encoding='utf-8') as f:
-  input = f.read().rstrip().split('\n')
-input = list(map(lambda s: s.strip(), input))
-
-
 # Part 1:
-def part1(input = None):
+def part1(input = None) -> str|int:
   return 0
 
 
 # Part 2:
-def part2(input = None):
+def part2(input = None) -> str|int:
   return 0
 
+def get_input():
+  # TODO: change the path of input file!
+  with open('day01/input', 'r', encoding='utf-8') as f:
+    input = [s.strip() for s in f.read().rstrip().split('\n')]
+  return input
 
 if __name__ == "__main__":
+  input = get_input()
   print("Part 1:", part1(input))
   print("Part 2:", part2(input))
