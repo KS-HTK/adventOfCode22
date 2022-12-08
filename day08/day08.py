a6a5f175a4c1ed9b0fc1b466ea606422e05d0465
KS_HTK <HKoertge@gmx.de>
2022-12-08 15:45:23 +0100

    updated day08 changed to use less loops

--- day08/day08.py
+++ day08/day08.py
@@ -1,85 +1,74 @@
 # -*- coding: utf-8 -*-
 
-from typing import List
+from typing import List, Set
 from math import prod
 
 # Part 1:
-def part1(input: List[List[int]]) -> int:
-  visible: int = 0
-  for y, a in enumerate(input):
-    for x, b in enumerate(a):
-      # Up
-      seen: bool = True
-      i = y-1
-      while i >= 0:
-        if b <= input[i][x]:
-          seen = False
-          break
-        i -= 1
-      if seen:
-        visible += 1
-        continue
-      # Down
-      seen = True
-      i = y+1
-      while i < len(input):
-        if b <= input[i][x]:
-          seen = False
-          break
-        i += 1
-      if seen:
-        visible += 1
-        continue
-      # Left
-      seen = True
-      i = x-1
-      while i >= 0:
-        if b <= a[i]:
-          seen = False
-          break
-        i -= 1
-      if seen:
-        visible += 1
-        continue
-      # Right
-      seen = True
-      i = x+1
-      while i < len(a):
-        if b <= a[i]:
-          seen = False
-          break
-        i += 1
-      if seen:
-        visible += 1
-  return visible
+def part1(height: List[List[int]]) -> int:
+  visible: Set[(int, int)] = set()
+  # This code is inspired by glor8 js code. 
+  #  For my original version see prior commit.
+  for y, line in enumerate(height): # iterate t -> d
+    lr = rl = -1
+    for x in range(len(line)): # iterate l -> r
+      nx = (len(line)-1)-x #index from end
+      if lr < line[x]: #l->r
+        visible.add((x, y))
+        lr = line[x]
+      if rl < line[nx]: #r->l
+        visible.add((nx, y))
+        rl = line[nx]
+  # assert that all lists in list are equal lenght
+  assert len(set([len(l) for l in height])) == 1 
+  for x in range(len(height[0])): # iterate l -> r
+    tb = bt = -1
+    for y in range(len(height)): # iterate t -> d
+      ny = (len(height)-1)-y #index from end
+      if tb < height[y][x]: #t->b
+        visible.add((x, y))
+        tb = height[y][x]
+      if bt < height[ny][x]: #b->t
+        visible.add((x, ny))
+        bt = height[ny][x]
+  return len(visible)
 
 # Part 2:
-def part2(input: List[List[int]]) -> int:
+def part2(height: List[List[int]]) -> int: # 405769
   max_seenic_score: int = 0
-  for y, a in enumerate(input):
-    for x, b in enumerate(a):
-      distance: List[int] = [0, 0, 0, 0]
-      # Up
-      i = y-1
-      while i >= 0 and b > input[i][x]:
-        i -= 1
-      distance[0] = y-i-1
-      # Down
-      i = y+1
-      while i < len(input) and b > input[i][x]:
+  assert len(height) == len(height[0])
+  lenh = max(len(height), len(height[0]))
+  for y in range(lenh):
+    for x in range(lenh):
+      h = height[y][x]
+      # t, b, l, r
+      visible_trees: List[int] = [0, 0, 0, 0]
+      view: List[bool] = [True, True, True, True]
+      i = 1
+      while True:
+        # -> t
+        if view[0] and y-i >= 0:
+          visible_trees[0] += 1
+        if y-i < 0 or height[y-i][x] >= h:
+          view[0] = False
+        # -> b
+        if view[1] and y+i < lenh:
+          visible_trees[1] += 1
+        if y+i >= lenh or height[y+i][x] >= h:
+          view[1] = False
+        # -> l
+        if view[2] and x-i >= 0:
+          visible_trees[2] += 1
+        if x-i < 0 or height[y][x-i] >= h:
+          view[2] = False
+        # -> r
+        if view[3] and x+i < lenh:
+          visible_trees[3] += 1
+        if x+i >= lenh or height[y][x+i] >= h:
+          view[3] = False
+        if True not in view:
+          break
         i += 1
-      distance[1] = i-y-1
-      # Left
-      i = x-1
-      while i >= 0 and b > a[i]:
-        i -= 1
-      distance[2] = x-i # not to sure why -1 not required here
-      # Right
-      i = x+1
-      while i < len(a) and b > a[i]:
-        i += 1
-      distance[3] = i-x-1
-      max_seenic_score = max(prod(distance), max_seenic_score)
+      max_seenic_score = max(prod(visible_trees), max_seenic_score)
   return max_seenic_score
 
 def get_input():
