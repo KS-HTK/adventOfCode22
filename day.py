65ecd08771408f91c4b9ed7d24dfcc8af85c587a
KS_HTK <HKoertge@gmx.de>
2022-12-10 08:25:27 +0100

    updated day.py Now use folder of script to find input file.

--- day.py
+++ day.py
@@ -6,14 +6,12 @@ from typing import List
 def part1(input = None) -> str|int:
   return 0
 
-
 # Part 2:
 def part2(input = None) -> str|int:
   return 0
 
 def get_input():
-  # TODO: change the path of input file!
-  with open('day01/input', 'r', encoding='utf-8') as f:
+  with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
     input = [s.strip() for s in f.read().rstrip().split('\n')]
   return input
 
