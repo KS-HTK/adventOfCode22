33a7de08befcc9ff815af879957e8f02ba6c4eb3
KS_HTK <HKoertge@gmx.de>
2022-12-10 08:15:09 +0100

    solved day09 (improved runtime)

--- day09/day09.py
+++ day09/day09.py
@@ -22,36 +22,24 @@ def solve(cmds: List[List[str]]) -> (int, int):
       l -= 1
       # Check if tail needs to move
       for i in range(1, len(tail)):
-        tail[i] = sim_tail(tail[i-1], tail[i])
+        diff = sub(tail[i-1], tail[i])
+        if not (abs(diff[0]) < 2 and abs(diff[1]) < 2):
+          tail[i] = add(tail[i], sign(diff))
       tail1_visited.add(tail[1])
       tail9_visited.add(tail[9])
   return (len(tail1_visited), len(tail9_visited))
 
-def sim_tail(head: Tuple[int, int], tail: Tuple[int, int]) -> Tuple[int, int]:
-    moves: Dict[Tuple[int, int], Tuple[int, int]] = move(head)
-    if tail in moves.keys():
-      # tail needs to move
-      return moves[tail]
-    return tail
-
-def move(t: Tuple[int, int]) -> Dict[Tuple[int, int], Tuple[int, int]]:
-  moves: Dict[Tuple[int, int], Tuple[int, int]] = {}
-  for dx in range(-2, 3):
-    for dy in range(-2, 3):
-      if dx not in [-2, 2] and dy not in [-2, 2]:
-        continue
-      moves[add(t, (dx, dy))] = add(t, (sign(dx), sign(dy)))
-      if (abs(dx) == 2 and abs(dy) == 1) or (abs(dx) == 1 and abs(dy) == 2):
-        moves[add(t, (dx, dy))] = add(t, (dx-sign(dx), dy-sign(dy)))
-  return moves
-
-# tuple addition
+# tuple addition and subtraction
 def add(t1: Tuple[int, int], t2: Tuple[int, int]) -> Tuple[int, int]:
   return tuple(map(lambda i, j: i + j, t1, t2))
 
+def sub(t1: Tuple[int, int], t2: Tuple[int, int]) -> Tuple[int, int]:
+  return tuple(map(lambda i, j: i - j, t1, t2))
+
 # sign function
-def sign(x: int) -> int:
-  return -1 if x < 0 else 0 if x == 0 else 1
+def sign(t: Tuple[int, int]) -> Tuple[int, int]:
+  s = lambda x: 0 if x == 0 else x // abs(x)
+  return tuple(map(s, t))
 
 def get_input() -> List[List[str]]:
   with open('day09/input', 'r', encoding='utf-8') as f:
