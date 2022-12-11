ff913e12b83c81460f59efa6e500256007e90b94
KS_HTK <HKoertge@gmx.de>
2022-12-11 10:12:36 +0100

    solved day11 (cleaned code)

--- day11/day11.py
+++ day11/day11.py
@@ -2,64 +2,60 @@
 
 import os
 import re
-from typing import List
+from typing import List, Callable
 
 class Monkey():
-  def __init__(self, monkey_str: List[str]):
-    nums = lambda s: map(int, re.findall(r'\d+', s))
-    sint = lambda s: int(re.search(r'\d+', s).group())
-    self.id = sint(monkey_str[0])
-    self.items = list(nums(monkey_str[1]))
-    self.operation = monkey_str[2]
-    self.testVal = sint(monkey_str[3])
-    self.test = lambda x: x % self.testVal == 0
-    self.trueID = sint(monkey_str[4])
-    self.falseID = sint(monkey_str[5])
-    self.true = None
-    self.false = None
-    self.activity = 0
-    self.parse_Op()
+  def __init__(self, monkey_str: List[str]) -> None:
+    nums: Callable[[str], List[int]] = lambda s: map(int, re.findall(r'\d+', s))
+    sint: Callable[[str], int] = lambda s: int(re.search(r'\d+', s).group())
+    self.id: int = sint(monkey_str[0])
+    self.items: List[int] = list(nums(monkey_str[1]))
+    self.operation_str: str = monkey_str[2].split(' = ')[1].replace('old', 'x')
+    self.operation: Callable[[int], int] = self.parse_Op()
+    self.testVal: int = sint(monkey_str[3])
+    self.test: Callable[[int], bool] = lambda x: x % self.testVal == 0
+    self.trueID: int = sint(monkey_str[4])
+    self.falseID: int = sint(monkey_str[5])
+    self.true: Monkey = None
+    self.false: Monkey = None
+    self.divisor: int = None
+    self.activity: int = 0
 
-  def set_true(self, m) -> None:
-    self.true = m
-  def set_false(self, m) -> None:
-    self.false = m
-  
-  def give(self, item: int) -> None:
-    self.items.append(item)
-
-  def parse_Op(self) -> None:
-    op = self.operation.split(' = ')[1].split(' ')
+  def parse_Op(self) -> Callable[[int], int]:
+    op = self.operation_str.split(' ')
     if op[1] == '+':
-      if op[2] == 'old':
-        self.operation = lambda x: x + x
+      if op[2] == 'x':
+        return lambda x: x + x
       else:
-        self.operation = lambda x: x + int(op[2])
+        return lambda x: x + int(op[2])
     elif op[1] == '*':
-      if op[2] == 'old':
-        self.operation = lambda x: x * x
+      if op[2] == 'x':
+        return lambda x: x * x
       else:
-        self.operation = lambda x: x * int(op[2])
+        return lambda x: x * int(op[2])
+    else:
+      # function could be:
+      return lambda x: eval(self.operation_str)
+      # but this is very slow in execution, so only used if not manualy parsed
 
-  def next_item(self, pt2, divisor) -> int:
+  def next_item(self, pt2: bool) -> int:
     item = self.operation(self.items.pop(0))
     if not pt2:
       item = item//3
     self.activity += 1
     if self.test(item):
-      self.true.give(item%divisor if pt2 else item)
+      self.true.items.append(item%self.divisor if pt2 else item)
     else:
-      self.false.give(item%divisor if pt2 else item)
+      self.false.items.append(item%self.divisor if pt2 else item)
 
-  def do_round(self, pt2 = False, divisor = 1) -> None:
+  def do_round(self, pt2: bool = False) -> None:
     while len(self.items) > 0:
-      self.next_item(pt2, divisor)
-    assert len(self.items) == 0
-    
+      self.next_item(pt2)
+
 
 # Part 1:
-def part1(monkey_list: List[object]) -> int:
-  for i in range(20):
+def part1(monkey_list: List[Monkey]) -> int:
+  for _ in range(20):
     for m in monkey_list:
       m.do_round()
 
@@ -67,39 +63,35 @@ def part1(monkey_list: List[object]) -> int:
   return monkey_list[-1].activity * monkey_list[-2].activity
 
 # Part 2:
-def part2(monkey_list: List[object]) -> int:
-  divisor: int = 1
-  for m in monkey_list:
-    divisor *= m.testVal
-
+def part2(monkey_list: List[Monkey]) -> int:
   for _ in range(10000):
     for m in monkey_list:
-      m.do_round(pt2 = True, divisor=divisor)
+      m.do_round(pt2 = True)
 
-  monkey_list.sort(key=lambda m : m.activity)
+  monkey_list.sort(key=lambda m: m.activity)
   return monkey_list[-1].activity * monkey_list[-2].activity
 
-def get_input():
+def get_input() -> List[str]:
   with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
-    input = [s.split('\n') for s in f.read().rstrip().split('\n\n')]
-  return input
+    content = [s.split('\n') for s in f.read().rstrip().split('\n\n')]
+  return content
 
 if __name__ == "__main__":
-  input = get_input()
-  monkeys = list(input)
-  monkeys2 = list(input)
-  for monkey in input:
-    m = Monkey(monkey)
-    m2 = Monkey(monkey)
-    i = m.id
-    monkeys[i] = m
-    monkeys2[i] = m2
-  for m in monkeys:
-    m.set_true(monkeys[m.trueID])
-    m.set_false(monkeys[m.falseID])
-  for m in monkeys2:
-    m.set_true(monkeys2[m.trueID])
-    m.set_false(monkeys2[m.falseID])
+  content: List[str] = get_input()
+  monkeys1: List[Monkey] = [None]*len(content)
+  monkeys2: List[Monkey] = [None]*len(content)
+  divisor: int = 1
+  for part in content:
+    m1 = Monkey(part)
+    m2 = Monkey(part)
+    divisor *= m2.testVal
+    monkeys1[m1.id] = m1
+    monkeys2[m2.id] = m2
+  for m_l in [monkeys1, monkeys2]:
+    for m in m_l:
+      m.divisor = divisor
+      m.true = m_l[m.trueID]
+      m.false = m_l[m.falseID]
 
-  print("Part 1:", part1(monkeys))
+  print("Part 1:", part1(monkeys1))
   print("Part 2:", part2(monkeys2))
