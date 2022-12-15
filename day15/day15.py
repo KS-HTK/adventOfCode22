bf6faccfdb227260447d6d067382e65f7ffc0eb9
KS_HTK <HKoertge@gmx.de>
2022-12-15 22:34:49 +0100

    added graphical solution for the example

--- day15/day15.py
+++ day15/day15.py
@@ -38,8 +38,8 @@ def part2(content: List[Tuple[int, int, int]]) -> int:
           if (all(abs(ux - vx) + abs(uy - vy) > mdv for vx, vy, mdv in content)):
             return int(ux * 4000000 + uy)
 
-def get_input():
-  with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
+def get_input(suffix=''):
+  with open(os.path.dirname(os.path.realpath(__file__))+'/input'+suffix, 'r', encoding='utf-8') as f:
     content = [s.strip() for s in f.read().rstrip().split('\n')]
   ints = [list(map(int, re.findall("-?\d+", l))) for l in content]
   return [(l[0], l[1], int(abs(l[0]-l[2])+abs(l[1]-l[3]))) for l in ints]
@@ -50,5 +50,36 @@ def solve():
   print("Part 1:", part1(content))
   print("Part 2:", part2(content))
 
+# Alternative solution for part 1 & 2: !!!! SLOW !!!!
+# we will render each line as a string and then search for the '#.#' pattern
+# ONLY WORKS FOR THE EXAMPLE INPUT
+@profiler
+def solve2() -> None:
+  sensors: List[Tuple[int, int, int]] = get_input('.txt')
+  TARGET_Y: int = 10
+  xy_max: int = 0
+  for sx, sy, md in sensors:
+    xy_max = max(xy_max, abs(sx+md), abs(sx-md), abs(sy+md), abs(sy-md))
+  xy_max+=1
+  grid: Dict[int, str] = dict()
+  for y in range(-xy_max, xy_max):
+    grid[y] = (''.join(['#' if any(abs(x - sx) + abs(y - sy) <= md for sx, sy, md in sensors) else '.' for x in range(-xy_max, xy_max)]))
+  dot_line = (''.join(['.' for _ in range(-xy_max, xy_max)]))
+  while dot_line in grid.values():
+    grid.pop([k for k, v in grid.items() if v == dot_line][0])
+  min_x: int = min([l.index('#') for l in grid.values()])
+  max_x: int = max([l.rindex('#') for l in grid.values()])
+  grid: Dict[int, str] = {y: l[min_x:max_x+1] for y, l in grid.items()} 
+  print("Part 1:", grid[TARGET_Y].count('#')-1) # -1 because there is a beacon in y=10
+  # find the '.' surrounded by '#' in the grid
+  for y, l in grid.items():
+    if '#.#' in l:
+      x = grid[y].index('#.#')+1
+      if grid[y-1][x] == '#' and grid[y+1][x] == '#':
+        print("Part 2:", (x+min_x-xy_max) * 4000000 + y)
+        break
+  print('\n'.join([''.join(l) for l in grid.values()]))
+
 if __name__ == "__main__":
   solve()
+  solve2()
