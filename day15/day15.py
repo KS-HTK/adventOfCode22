426b8b5ac138f093b5714554d3d533f214207244
KS_HTK <HKoertge@gmx.de>
2022-12-15 07:54:23 +0100

    fixed incorrect type hinting removed use of complex

--- day15/day15.py
+++ day15/day15.py
@@ -13,41 +13,41 @@ def profiler(method):
     return ret
   return profiler_method
 
-def part1(content: List[Tuple[complex, int]]) -> int:
-  TARGET_Y = 2000000
+def part1(content: List[Tuple[int, int, int]]) -> int:
+  TARGET_Y: int = 2000000
   xl: int = 40000000
   xh: int = -40000000
-  for s, md in content:
-    dx: int = md - int(abs(TARGET_Y - s.imag))
+  for sx, sy, md in content:
+    dx: int = md - int(abs(TARGET_Y - sy))
     if dx > 0:
-      xl = min(xl, int(s.real - dx))
-      xh = max(xh, int(s.real + dx))
+      xl = min(xl, int(sx - dx))
+      xh = max(xh, int(sx + dx))
   return xh - xl
 
 # Part 2:
-def part2(content: List[List[complex]]) -> int:
-  for s, md in content:
+def part2(content: List[Tuple[int, int, int]]) -> int:
+  max_x: int = 4000000
+  for sx, sy, md in content:
     for p in range(md + 1):
-      for tx, ty in ((s.real - md - 1 + p, s.imag - p),
-                     (s.real + md + 1 - p, s.imag - p),
-                     (s.real - md - 1 + p, s.imag + p),
-                     (s.real + md + 1 - p, s.imag + p)):
-        if (0 <= tx <= 4000000 and
-            0 <= ty <= 4000000 and
-            all(abs(tx - s2.real) + abs(ty - s2.imag) > md2
-                for s2, md2 in content)):
-          return int(tx * 4000000 + ty)
+      for tx, ty in ((sx - md - 1 + p, sy - p),
+                     (sx + md + 1 - p, sy - p),
+                     (sx - md - 1 + p, sy + p),
+                     (sx + md + 1 - p, sy + p)):
+        if (0 <= tx <= max_x and
+            0 <= ty <= max_x and
+            all(abs(tx - sx2) + abs(ty - sy2) > md2
+                for sx2, sy2, md2 in content)):
+          return int(tx * max_x + ty)
 
 def get_input():
   with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
     content = [s.strip() for s in f.read().rstrip().split('\n')]
   ints = [list(map(int, re.findall("-?\d+", l))) for l in content]
-  c = lambda x, y: complex(x, y)
-  return [(c(l[0], l[1]), int(abs(l[0]-l[2])+abs(l[1]-l[3]))) for l in ints]
+  return [(l[0], l[1], int(abs(l[0]-l[2])+abs(l[1]-l[3]))) for l in ints]
 
 @profiler
 def solve():
-  content = get_input()
+  content: List[Tuple[complex, int]] = get_input()
   print("Part 1:", part1(content)) # 5083287
   print("Part 2:", part2(content)) # 13134039205729
 
