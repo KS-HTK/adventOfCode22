a40e57b6a56580060965cd897222a5cee5f4d906
KS_HTK <HKoertge@gmx.de>
2022-12-17 02:44:37 +0100

    alternate faster solution for pt2

--- day16/day16.py
+++ day16/day16.py
@@ -14,6 +14,7 @@ def profiler(method):
   return profiler_method
 
 # Part 1:
+@profiler
 def part1(valves: Dict[str, Dict]) -> int:
   best = 0
   def search(opened, flowed, pos, time_rem):
@@ -51,6 +52,60 @@ def part2(valves: Dict[str, Dict]) -> int:
   search(set(['AA']), 0, 'AA', 25, False)
   return best
 
+# Aternative to part 1 & 2:
+# Way faster than part 2, but not as fast as part 1
+# Idea curtesy of Xodarap
+@profiler
+def calc(valves: Dict[str, Dict], time: int = 30) -> int:
+  pathset = set()
+  def generate_paths(pos, path, time_rem):
+    for k in [x for x in valves[pos]['paths'].keys() if x not in path]:
+      required_time = valves[pos]['paths'][k]+1 # go to destination + open valve
+      if time_rem - required_time <= 0:
+        continue
+      pathset.add(tuple(path+[k]))
+      generate_paths(k, path + [k], time_rem - required_time)
+  generate_paths('AA', ['AA'], time)
+
+  def e_paths(humanpath):
+    e_pathset = set()
+    def elephant_paths(pos, path, time_rem, humanpath):
+      for k in [x for x in valves[pos]['paths'].keys() if x not in path and x not in humanpath]:
+        required_time = valves[pos]['paths'][k]+1 # go to destination + open valve
+        if time_rem - required_time <= 0:
+          continue
+        e_pathset.add(tuple(path+[k]))
+        elephant_paths(k, path + [k], time_rem - required_time, humanpath)
+    elephant_paths('AA', ['AA'], time, humanpath)
+    return e_pathset
+  
+  def get_flow(path):
+    flow = 0
+    _time = time
+    for i in range(len(path)-1):
+      _time -= valves[path[i]]['paths'][path[i+1]] # go to destination
+      _time -= 1 # open valve
+      flow += valves[path[i+1]]['flow']*_time
+      if _time <= 0:
+        print("Time ran out")
+        return -1
+    return flow
+  
+  optimum = max(get_flow(x) for x in pathset)
+  if time == 30:
+    return optimum
+
+  # Part 2:
+  max_flow = 0
+  for path in pathset:
+    h_flow = get_flow(path)
+    if h_flow < max_flow - optimum:
+      continue
+    e_flow = max(get_flow(x) for x in e_paths(path))
+    if h_flow + e_flow > max_flow:
+      max_flow = h_flow + e_flow
+  return max_flow
+
 def bfs(valves: Dict[str, Dict], front: Set[str], end: str) -> int:
   depth: int = 1
   while True:
@@ -84,7 +139,7 @@ def solve():
       if k2 != k:
         valves[k]['paths'][k2] = bfs(valves, valves[k]['tunnels'], k2)
   print("Part 1:", part1(valves))
-  print("Part 2:", part2(valves))
+  print("Part 2:", calc(valves, 26))
 
 if __name__ == "__main__":
   solve()
