c9a4508ca62f64b5139618d42844fa9aa79c9e25
hkoertge <hjalmar.koertge@iee.fraunhofer.de>
2022-12-20 13:48:47 +0100

    added profiler to timeall

--- timeall.py
+++ timeall.py
@@ -1,13 +1,26 @@
 from time import perf_counter
 
-times = {}
-for i in range(20):
-  name = f'day{i+1:02d}.day{i+1:02d}'
-  day = __import__(name, fromlist=[''])
+def profiler(method):
+  def profiler_method(*arg, **kw):
+    t = perf_counter()
+    ret = method(*arg, **kw)
+    print(f'{method.__name__} method took : {perf_counter()-t:.4f} sec')
+    return ret
+  return profiler_method
 
-  t = perf_counter()
-  day.solve()
-  times[perf_counter()-t] = name
+@profiler
+def timeall():
+  times = {}
+  for i in range(20):
+    name = f'day{i+1:02d}.day{i+1:02d}'
+    day = __import__(name, fromlist=[''])
 
-max_time = max(times.keys())
-print(times[max_time], f'{max_time:,.4f}', 'sec')
+    t = perf_counter()
+    day.solve()
+    times[perf_counter()-t] = name
+
+  max_time = max(times.keys())
+  print(times[max_time], f'{max_time:,.4f}', 'sec')
+
+if __name__ == "__main__":
+  timeall()
