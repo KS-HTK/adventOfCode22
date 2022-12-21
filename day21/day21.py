ed4491bcbb0fbfaf5fb3a81d05d155f03e0dedf2
KS_HTK <HKoertge@gmx.de>
2022-12-21 20:45:17 +0100

    removed double calculation

--- day21/day21.py
+++ day21/day21.py
@@ -14,27 +14,29 @@ def profiler(method):
   return profiler_method
 
 # Part 1:
-def part1(content: Dict[str, int|Tuple[str, str, Callable[[int, int], int]]]) -> int:
-  while not isinstance(content['root'], int):
-    for k, v in content.items():
-      if isinstance(v, int):
-        continue
-      if isinstance(content[v[0]], int) and isinstance(content[v[1]], int):
-        content[k] = round(v[2](content[v[0]], content[v[1]]))
-  return content['root']
+def part1(r1: complex, r2: complex, rO: Callable[[complex, complex], complex], humn: complex) -> int:
+  res = rO(r1, r2)
+  res = res.real+ float(res.imag)*humn.real
+  return round(res)
 
 # Part 2: (original implementation)
-def original_part2(content: List[complex|Tuple[str, str, Callable[[complex, complex], complex]]]) -> str:
+def original_part2(r1: complex, r2: complex) -> str:
+  res = r1 - r2
+  res = res.real / float(res.imag)
+  return abs(round(res))
+
+def calc(content: List[complex|Tuple[str, str, Callable[[complex, complex], complex]]]) -> str:
   content['humn'] = 1j
+  r1, r2, _ = content['root']
+  if isinstance(content[r1], complex) and isinstance(content[r2], complex):
+    return content[r1], content[r2]
   while not isinstance(content['root'], complex):
     for k, v in content.items():
       if isinstance(v, complex):
         continue
       if (isinstance(content[v[0]], complex) and isinstance(content[v[1]], complex)):
         if k == 'root':
-          res = content[v[1]] - content[v[0]]
-          res = res.real / float(res.imag)
-          return abs(int(res))
+          return content[v[1]], content[v[0]]
         content[k] = v[2](content[v[0]], content[v[1]])
 
 # Improved Part 2: (using sympy) I did this because my original implementation seems like a hack.
@@ -78,7 +80,7 @@ OPPMAP = {
 def get_input():
   with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
     content = [s.strip() for s in f.read().rstrip().split('\n')]
-  m1, m2, m3 = {}, {}, {}
+  m1, m2 = {}, {}
   for l in content:
     k, v = l.split(': ')
     try:
@@ -86,22 +88,23 @@ def get_input():
     except ValueError:
       pass
     else:
-      m1[k] = v
-      m2[k] = complex(v, 0)
-      m3[k] = v
+      m1[k] = complex(v, 0)
+      m2[k] = v
       continue
     v = v.split(' ')
     m1[k] = (v[0], v[2], OPPMAP[v[1]])
-    m2[k] = (v[0], v[2], OPPMAP[v[1]])
-    m3[k] = (v[0], v[2], v[1])
-  return m1, m2, m3
+    m2[k] = (v[0], v[2], v[1])
+  return m1, m2
 
 @profiler
 def solve():
-  m1, m2, m3 = get_input()
-  print("Part 1:", part1(m1))
-  print("Part 2:", original_part2(m2))
-  #print("Part 2:", part2(m3))
+  m1, m2 = get_input()
+  humn = m1['humn']
+  r1, r2 = calc(m1)
+  _, _, rO = m1['root']
+  print("Part 1:", part1(r1, r2, rO, humn))
+  print("Part 2:", original_part2(r1, r2))
+  #print("Part 2:", part2(m2))
   
 
 if __name__ == "__main__":
