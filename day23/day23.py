c77b121ef3e50c6ea014bcd0d7956b13eda9a089
KS_HTK <HKoertge@gmx.de>
2022-12-23 21:07:00 +0100

    improved day23.py fixed typo

--- day23/day23.py
+++ day23/day23.py
@@ -2,7 +2,7 @@
 
 import os
 from time import perf_counter
-from typing import List, Dict, Tuple, Generator
+from typing import List, Dict, Tuple, Generator, Set
 
 def profiler(method):
   def profiler_method(*arg, **kw):
@@ -24,84 +24,79 @@ NEIGHBOURS: List[complex] = [
 ]
 
 # global variables
-checks: List[List[complex]] = [
+dirs: List[List[complex]] = [
   [complex(0, -1), complex(-1, -1), complex(1, -1)], # N
   [complex(0, 1), complex(-1, 1), complex(1, 1)],  # S
   [complex(-1, 0), complex(-1, -1), complex(-1, 1)], # W
   [complex(1, 0), complex(1, -1), complex(1, 1)] # E
 ]
 
-# Part 1:
-def calc(elfs: List[complex]) -> Tuple[int, int]:
+# Part 1: Thanks to @Xodarap for the idea to use 3 sets instead of a large dict
+def calc(elves: Set[complex]) -> Tuple[int, int]:
   global checks
   _round: int = 0
   part1_solution: int = 0
   while True: # rounds
     _round += 1
-    moved_count = 0
     # first half: propose move
-    neoelfs: Dict[complex, List[complex]] = {}
-    for elf in elfs:
-      moved: bool = False
-      if any([f in elfs for f in get_neighbours(elf)]):
-        for direction in checks:
-          if all([elf + d not in elfs for d in direction]):
-            moved = True
-            moved_count += 1
-            neoelf = elf + direction[0]
-            if neoelf in neoelfs:
-              neoelfs[neoelf].append(elf)
-              break
-            else:
-              neoelfs[neoelf] = [elf]
-              break
-      if not moved:
-        neoelfs[elf] = [elf]
+    relations: Dict[complex, complex] = {}
+    props: Set[complex] = set()
+    dupes: Set[complex] = set()
+    for elf in elves:
+      if all(elf+n not in elves for n in NEIGHBOURS):
+        continue
+      prop: complex = None
+      for direction in dirs:
+        if all(elf + d not in elves for d in direction):
+          prop = elf + direction[0]
+          break
+      if prop == None:
+        continue
+      if prop in dupes:
+        continue
+      if prop in props:
+        dupes.add(prop)
+        continue
+      props.add(prop)
+      relations[elf] = prop
+
     # reset all elves that proposed the same move
-    for elf in list(neoelfs):
-      if len(neoelfs[elf]) > 1:
-        for e in neoelfs[elf]:
-          moved_count -= 1
-          neoelfs[e] = [e]
-        del neoelfs[elf]
-    # second half: move
-    elfs = neoelfs.keys()
-    checks.append(checks.pop(0)) # rotate directions list
+    relations = {k: v for k, v in relations.items() if v not in dupes}
+    neoelves: Set[complex] = set(relations.values())
+    remelves: Set[complex] = elves - set(relations.keys())
+    elves = neoelves | remelves
+    # check if no elf moved
+    if len(relations.values()) == 0:
+      break
+
+    dirs.append(dirs.pop(0)) # rotate directions list
     
     # save part1 solution
     if _round == 10:
-      part1_solution = count_free_fields(list(elfs))
-    # check if no elf moved
-    if moved_count == 0:
-      break
+      part1_solution = count_free_fields(elves)
   return part1_solution, _round
 
-def get_neighbours(elf: complex) -> Generator[complex, complex, None]:
-  for d in NEIGHBOURS:
-    yield elf + d
-
-def count_free_fields(elfs: List[complex]) -> int:
+def count_free_fields(elves: Set[complex]) -> int:
   # find min max
-  rlist: List[int] = [round(c.real) for c in elfs]
-  ilist: List[int] = [round(c.imag) for c in elfs]
-  x = 1+max(rlist) - min(rlist)
-  y = 1+max(ilist) - min(ilist)
-  return x*y-len(elfs)
+  r, i = zip(*[(e.real, e.imag) for e in elves])
+  x = 1+max(r) - min(r)
+  y = 1+max(i) - min(i)
+  return x*y-len(elves)
 
-def get_input() -> List[complex]:
-  with open(os.path.dirname(os.path.realpath(__file__))+'/input', 'r', encoding='utf-8') as f:
+def get_input() -> Set[complex]:
+  with open(os.path.dirname(os.path.realpath(__file__))+'/data_23.txt', 'r', encoding='utf-8') as f:
     content = [[c for c in s.strip()] for s in f.read().rstrip().split('\n')]
-  elfs = []
+  elves: Set[complex] = set()
   for y, l in enumerate(content):
     for x, c in enumerate(l):
       if c == '#':
-        elfs.append(complex(x, y))
-  return elfs
+        elves.add(complex(x, y))
+  return elves
 
 @profiler
 def solve():
-  elfs: List[complex] = get_input()
-  print("Part 1: %d\nPart 2: %d" % calc(elfs))
+  elves: Set[complex] = get_input()
+  print("Part 1: %d\nPart 2: %d" % calc(elves))
 
 if __name__ == "__main__":
   solve()
